37eb42d204dca9ef645f2bd113cf5a4a194d4fd2
Yulia <yulia.scherbina@gmail.com>
2017-02-18 12:10:23 +0100

    add products in store

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,9 +1,25 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-  // app.controller('StoreController', function() {
-  //   this.products = gems;
+  // app.controller('GalleryController', function() {
+  //   this.current = 0;
+  //   this.setCurrent = function setCurrent(value) {
+  //     this.current = value || 0;
+  //   };
   // });
+  app.controller('GalleryController', function() {
+     this.imageIndex = 0;
+    console.log(this);
+     this.currentImageChange = function(imageNumber) {
+       console.log(imageNumber);
+       this.imageIndex = imageNumber || 0;
+     };
+ });
+
+ // app.controller('StoreController', function() {
+ //   this.products = gems;
+ // });
+
   app.controller('StoreController', ['$http', function($http) {
     var store = this;
     store.products = [];
--- app/static/js/products.js
+++ app/static/js/products.js
@@ -1,83 +1,122 @@
-(function() {
-  var app = angular.module('store-directives', []);
+var app = angular.module('store-directives', []);
 
-  // app.controller('TabController', function() {
-  //   this.tab = 1;
-  //
-  //   this.setTab = function(setTab) {
-  //     this.tab = setTab;
-  //   };
-  //   this.isSet = function(isSet) {
-  //     return this.tab === isSet;
-  //   };
-  // });
+app.directive("descriptions", function() {
+  return {
+    restrict: "E",
+    templateUrl: "includes/descriptions.html"
+  };
+});
 
-  app.directive('productTabs', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'includes/product_tabs.html',
-      controller: function() {
-        this.tab = 1;
+app.directive("reviews", function() {
+  return {
+    restrict: "E",
+    templateUrl: "includes/reviews.html"
+  };
+});
 
-        this.setTab = function(setTab) {
-          this.tab = setTab;
-        };
-        this.isSet = function(isSet) {
-          return this.tab === isSet;
-        };
-      },
-      controllerAs: "tab"
-    };
-  });
+app.directive("specs", function() {
+  return {
+    restrict: "E",
+    templateUrl: "includes/specs.html"
+  };
+});
 
-  app.controller('GalleryController', function() {
-    this.current = 0;
-    this.setCurrent = function setCurrent(value) {
-      this.current = value || 0;
-    };
-  });
+app.directive("productTabs", function() {
+  return {
+    restrict: "E",
 
-  // app.directive('productGallery', function(){
-  //   return {
-  //     restrict: 'E',
-  //     templateUrl: 'includes/product_gallery.html',
-  //     controller: function() {
-  //       this.current = 0;
-  //       this.setCurrent = function setCurrent(value) {
-  //         this.current = value || 0;
-  //       };
-  //     },
-  //     controllerAs: "gallery"
-  //   };
-  // });
+    templateUrl: "includes/product_tabs.html",
+    controller: function() {
+      this.tab = 1;
 
-  app.directive('productTitle', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'includes/product_title.html'
-    };
-  });
+      this.isSet = function(checkTab) {
+        return this.tab === checkTab;
+      };
 
-  app.directive('productDescription', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'includes/product_description.html'
-    };
-  });
-
-  app.directive('productSpecs', function(){
-    return {
-      restrict: 'A',
-      templateUrl: 'includes/product_specs.html'
-    };
-  });
-
-  app.directive('productReview', function(){
-    return {
-      restrict: 'E',
-      templateUrl: 'includes/product_review.html'
-    };
-  });
+      this.setTab = function(activeTab) {
+        this.tab = activeTab;
+      };
+    },
+    controllerAs: "tab"
+  };
+});
 
 
-})();
+// (function() {
+//   var app = angular.module('store-directives', []);
+//
+//   // app.controller('TabController', function() {
+//   //   this.tab = 1;
+//   //
+//   //   this.setTab = function(setTab) {
+//   //     this.tab = setTab;
+//   //   };
+//   //   this.isSet = function(isSet) {
+//   //     return this.tab === isSet;
+//   //   };
+//   // });
+//
+//   app.directive('productTabs', function(){
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'includes/product_tabs.html',
+//       controller: function() {
+//         this.tab = 1;
+//
+//         this.setTab = function(setTab) {
+//           this.tab = setTab;
+//         };
+//         this.isSet = function(isSet) {
+//           return this.tab === isSet;
+//         };
+//       },
+//       controllerAs: "tab"
+//     };
+//   });
+//
+//
+//
+//   // app.directive('productGallery', function(){
+//   //   return {
+//   //     restrict: 'E',
+//   //     templateUrl: 'includes/product_gallery.html',
+//   //     controller: function() {
+//   //       this.current = 0;
+//   //       this.setCurrent = function setCurrent(value) {
+//   //         this.current = value || 0;
+//   //       };
+//   //     },
+//   //     controllerAs: "gallery"
+//   //   };
+//   // });
+//
+//   app.directive('productTitle', function(){
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'includes/product_title.html'
+//     };
+//   });
+//
+//   app.directive('productDescription', function(){
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'includes/product_description.html'
+//     };
+//   });
+//
+//   app.directive('productSpecs', function(){
+//     return {
+//       restrict: 'A',
+//       templateUrl: 'includes/product_specs.html'
+//     };
+//   });
+//
+//   app.directive('productReview', function(){
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'includes/product_review.html'
+//     };
+//   });
+//
+//
+// })();
